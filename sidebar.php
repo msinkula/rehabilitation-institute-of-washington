2de299c73cc91e5ce833febe879b80fe9068e622
Mike Sinkula <mike@mikesinkula.com>
2017-01-20 15:20:02 -0800

    Created link to parent page in sub-navigation

--- sidebar.php
+++ sidebar.php
@@ -7,19 +7,20 @@
 	if ($post->post_parent) { // if has a post parent
             
 		$children = wp_list_pages("title_li=&child_of=".$post->post_parent."&echo=0" ); 
-		$parentname = get_the_title($post->post_parent);
+		$parentName = get_the_title($post->post_parent);
+        $parentLink = get_permalink($post->post_parent);
 
 	} else { // if does not have a post parent
 
 		$children = wp_list_pages("title_li=&child_of=".$post->ID."&echo=0" ); 
-		$parentname = get_the_title($post->ID);
+		$parentName = get_the_title($post->ID);
 	
 	}
 
 	if ($children) { // if has children ?>
 
         <div id="sub-navigation" class="widget-items">
-        <h2 class="sub-navigation-title"><?php echo $parentname; ?></h2>
+        <h2 class="sub-navigation-title"><a href="<?php echo $parentLink; ?>"><?php echo $parentName; ?></a></h2>
         <ul class="sub-navigation-items">
         <?php echo $children; ?>
         </ul>
